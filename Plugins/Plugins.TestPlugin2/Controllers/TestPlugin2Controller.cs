859bf632b07b128c07688a61db1ca2202dfb84be
Bilal AFŞAR <bafsar@users.noreply.github.com>
2019-08-22 05:37:45 +0300

    Add files via upload

---
+++ Plugins/Plugins.TestPlugin2/Controllers/TestPlugin2Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Plugins.TestPlugin2.Models;
+
+namespace Plugins.TestPlugin2.Controllers
+{
+    public class TestPlugin2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string StringTest()
+        {
+            return "Test Value";
+        }
+
+        public IActionResult JsonTest()
+        {
+            return Json(new SampleModelList());
+        }
+    }
+}
---
+++ Plugins/Plugins.TestPlugin2/Models/SampleModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Plugins.TestPlugin2.Models
+{
+    public class SampleModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class SampleModelList : List<SampleModel>
+    {
+        public SampleModelList()
+        {
+            Add(new SampleModel() { Id = 1, Title = "Title 1" });
+            Add(new SampleModel() { Id = 2, Title = "Title 2" });
+            Add(new SampleModel() { Id = 3, Title = "Title 3" });
+            Add(new SampleModel() { Id = 4, Title = "Title 4" });
+            Add(new SampleModel() { Id = 5, Title = "Title 5" });
+        }
+    }
+
+}
