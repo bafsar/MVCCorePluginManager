f999b8bf860ca2641263a9d17515c3a1515cd8f3
Bilal AFŞAR <bafsar@users.noreply.github.com>
2019-08-22 05:40:32 +0300

    Add files via upload

---
+++ Plugins/Plugins.TestPlugin1/Controllers/TestPlugin1Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Plugins.TestPlugin1.Controllers
+{
+    public class TestPlugin1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult OtherPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Plugins/Plugins.TestPlugin1/TestPlugin1Module.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using MVCCorePluginManager.Abstract;
+
+namespace Plugins.TestPlugin1
+{
+    public class TestPlugin1Module : IModule
+    {
+        public string Title
+        {
+            get { return "TestPlugin1 Index Page"; }
+        }
+
+        public string Name
+        {
+            get { return Assembly.GetAssembly(GetType()).GetName().Name; }
+        }
+
+        public Version Version
+        {
+            get { return new Version(1, 0, 0, 0); }
+        }
+
+        public string EntryControllerName
+        {
+            get { return "TestPlugin1"; }
+        }
+    }
+}
